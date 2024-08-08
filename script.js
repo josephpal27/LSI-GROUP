8da28b26782962687159fc3f0984b59e977b142d
Joseph Pal <josephpal27@gmail.com>
2024-08-08 16:11:41 +0530

    contents updated

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ let img1 = document.getElementById('img-1');
 img1.addEventListener('mouseenter', () => {
     img1.style.opacity = '0';
     setTimeout(() => {
-        img1.setAttribute('src', 'images/expertise/1(1).png');
+        img1.setAttribute('src', 'images/expertise/1(1).webp');
         img1.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -121,7 +121,7 @@ img1.addEventListener('mouseenter', () => {
 img1.addEventListener('mouseleave', () => {
     img1.style.opacity = '0';
     setTimeout(() => {
-        img1.setAttribute('src', 'images/expertise/1.png');
+        img1.setAttribute('src', 'images/expertise/1.webp');
         img1.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -132,7 +132,7 @@ let img2 = document.getElementById('img-2');
 img2.addEventListener('mouseenter', () => {
     img2.style.opacity = '0';
     setTimeout(() => {
-        img2.setAttribute('src', 'images/expertise/2(1).png');
+        img2.setAttribute('src', 'images/expertise/2(1).webp');
         img2.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -140,7 +140,7 @@ img2.addEventListener('mouseenter', () => {
 img2.addEventListener('mouseleave', () => {
     img2.style.opacity = '0';
     setTimeout(() => {
-        img2.setAttribute('src', 'images/expertise/2.png');
+        img2.setAttribute('src', 'images/expertise/2.webp');
         img2.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -151,7 +151,7 @@ let img3 = document.getElementById('img-3');
 img3.addEventListener('mouseenter', () => {
     img3.style.opacity = '0';
     setTimeout(() => {
-        img3.setAttribute('src', 'images/expertise/3(1).png');
+        img3.setAttribute('src', 'images/expertise/3(1).webp');
         img3.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -159,7 +159,7 @@ img3.addEventListener('mouseenter', () => {
 img3.addEventListener('mouseleave', () => {
     img3.style.opacity = '0';
     setTimeout(() => {
-        img3.setAttribute('src', 'images/expertise/3.png');
+        img3.setAttribute('src', 'images/expertise/3.webp');
         img3.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -170,7 +170,7 @@ let img4 = document.getElementById('img-4');
 img4.addEventListener('mouseenter', () => {
     img4.style.opacity = '0';
     setTimeout(() => {
-        img4.setAttribute('src', 'images/expertise/4(1).png');
+        img4.setAttribute('src', 'images/expertise/4(1).webp');
         img4.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -178,7 +178,7 @@ img4.addEventListener('mouseenter', () => {
 img4.addEventListener('mouseleave', () => {
     img4.style.opacity = '0';
     setTimeout(() => {
-        img4.setAttribute('src', 'images/expertise/4.png');
+        img4.setAttribute('src', 'images/expertise/4.webp');
         img4.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -189,7 +189,7 @@ let img5 = document.getElementById('img-5');
 img5.addEventListener('mouseenter', () => {
     img5.style.opacity = '0';
     setTimeout(() => {
-        img5.setAttribute('src', 'images/expertise/5(1).png');
+        img5.setAttribute('src', 'images/expertise/5(1).webp');
         img5.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -197,7 +197,7 @@ img5.addEventListener('mouseenter', () => {
 img5.addEventListener('mouseleave', () => {
     img5.style.opacity = '0';
     setTimeout(() => {
-        img5.setAttribute('src', 'images/expertise/5.png');
+        img5.setAttribute('src', 'images/expertise/5.webp');
         img5.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -208,7 +208,7 @@ let img6 = document.getElementById('img-6');
 img6.addEventListener('mouseenter', () => {
     img6.style.opacity = '0';
     setTimeout(() => {
-        img6.setAttribute('src', 'images/expertise/6(1).png');
+        img6.setAttribute('src', 'images/expertise/6(1).webp');
         img6.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -216,7 +216,7 @@ img6.addEventListener('mouseenter', () => {
 img6.addEventListener('mouseleave', () => {
     img6.style.opacity = '0';
     setTimeout(() => {
-        img6.setAttribute('src', 'images/expertise/6.png');
+        img6.setAttribute('src', 'images/expertise/6.webp');
         img6.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -227,7 +227,7 @@ let img7 = document.getElementById('img-7');
 img7.addEventListener('mouseenter', () => {
     img7.style.opacity = '0';
     setTimeout(() => {
-        img7.setAttribute('src', 'images/expertise/7(1).png');
+        img7.setAttribute('src', 'images/expertise/7(1).webp');
         img7.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -235,7 +235,7 @@ img7.addEventListener('mouseenter', () => {
 img7.addEventListener('mouseleave', () => {
     img7.style.opacity = '0';
     setTimeout(() => {
-        img7.setAttribute('src', 'images/expertise/7.png');
+        img7.setAttribute('src', 'images/expertise/7.webp');
         img7.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -246,7 +246,7 @@ let img8 = document.getElementById('img-8');
 img8.addEventListener('mouseenter', () => {
     img8.style.opacity = '0';
     setTimeout(() => {
-        img8.setAttribute('src', 'images/expertise/8(1).png');
+        img8.setAttribute('src', 'images/expertise/8(1).webp');
         img8.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -254,7 +254,7 @@ img8.addEventListener('mouseenter', () => {
 img8.addEventListener('mouseleave', () => {
     img8.style.opacity = '0';
     setTimeout(() => {
-        img8.setAttribute('src', 'images/expertise/8.png');
+        img8.setAttribute('src', 'images/expertise/8.webp');
         img8.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -265,7 +265,7 @@ let img9 = document.getElementById('img-9');
 img9.addEventListener('mouseenter', () => {
     img9.style.opacity = '0';
     setTimeout(() => {
-        img9.setAttribute('src', 'images/expertise/9(1).png');
+        img9.setAttribute('src', 'images/expertise/9(1).webp');
         img9.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -273,7 +273,7 @@ img9.addEventListener('mouseenter', () => {
 img9.addEventListener('mouseleave', () => {
     img9.style.opacity = '0';
     setTimeout(() => {
-        img9.setAttribute('src', 'images/expertise/9.png');
+        img9.setAttribute('src', 'images/expertise/9.webp');
         img9.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -284,7 +284,7 @@ let img10 = document.getElementById('img-10');
 img10.addEventListener('mouseenter', () => {
     img10.style.opacity = '0';
     setTimeout(() => {
-        img10.setAttribute('src', 'images/expertise/10(1).png');
+        img10.setAttribute('src', 'images/expertise/10(1).webp');
         img10.style.opacity = '1';
     }, 170); // Adjust timing as neede10
 });
@@ -292,7 +292,7 @@ img10.addEventListener('mouseenter', () => {
 img10.addEventListener('mouseleave', () => {
     img10.style.opacity = '0';
     setTimeout(() => {
-        img10.setAttribute('src', 'images/expertise/10.png');
+        img10.setAttribute('src', 'images/expertise/10.webp');
         img10.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -303,7 +303,7 @@ let img11 = document.getElementById('img-11');
 img11.addEventListener('mouseenter', () => {
     img11.style.opacity = '0';
     setTimeout(() => {
-        img11.setAttribute('src', 'images/expertise/11(1).png');
+        img11.setAttribute('src', 'images/expertise/11(1).webp');
         img11.style.opacity = '1';
     }, 170); // Adjust timing as neede10
 });
@@ -311,7 +311,7 @@ img11.addEventListener('mouseenter', () => {
 img11.addEventListener('mouseleave', () => {
     img11.style.opacity = '0';
     setTimeout(() => {
-        img11.setAttribute('src', 'images/expertise/11.png');
+        img11.setAttribute('src', 'images/expertise/11.webp');
         img11.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
@@ -322,7 +322,7 @@ let img12 = document.getElementById('img-12');
 img12.addEventListener('mouseenter', () => {
     img12.style.opacity = '0';
     setTimeout(() => {
-        img12.setAttribute('src', 'images/expertise/12(1).png');
+        img12.setAttribute('src', 'images/expertise/12(1).webp');
         img12.style.opacity = '1';
     }, 170); // Adjust timing as neede10
 });
@@ -330,7 +330,7 @@ img12.addEventListener('mouseenter', () => {
 img12.addEventListener('mouseleave', () => {
     img12.style.opacity = '0';
     setTimeout(() => {
-        img12.setAttribute('src', 'images/expertise/12.png');
+        img12.setAttribute('src', 'images/expertise/12.webp');
         img12.style.opacity = '1';
     }, 170); // Adjust timing as needed
 });
